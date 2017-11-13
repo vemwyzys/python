59a104babed5c9699aee2e493a12943d0a866339
gzho <disanxinshi@hotmail.com>
2017-11-13 17:09:46 +0800

    1

--- pythonTest/1.py
+++ pythonTest/1.py
@@ -1,3 +1,7 @@
+#!/usr/bin/env python3
+#-*- coding:utf-8 -*-
+#第一行告诉macos和linux这是一个python执行程序，win会忽略它
+#第二行告诉系统这使用的是utf-8编码
 print(100+200+500/3)
 print('the sabi is saying the','big lie','haha')
 print('100+200+300=',100+200+300)
@@ -11,3 +15,5 @@ print('I\\ \'m sabi')
 print(r'''\\\\\\
 ...line
 ...line''')
+print(ord('a'))
+print(chr(99))
