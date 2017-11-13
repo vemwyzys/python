b5c4ff2ab72c4214b5f81a9096ce9838b9bc3e8e
gzho <disanxinshi@hotmail.com>
2017-11-13 16:29:11 +0800

    test

---
+++ pythonTest/1.py
@@ -0,0 +1,13 @@
+print(100+200+500/3)
+print('the sabi is saying the','big lie','haha')
+print('100+200+300=',100+200+300)
+#name=input('please input your name : ')
+#print('hello',name)
+
+print('1024 * 768 = ',1024*768)
+print('I\'m sabi')
+print('I\n \'m sabi')
+print('I\\ \'m sabi')
+print(r'''\\\\\\
+...line
+...line''')
