ea199139f4707b185b4cd55e019f0b9cae2895e1
gzho <disanxinshi@hotmail.com>
2017-11-20 17:07:04 +0800

    2017.11.20

--- pythonTest/2.py
+++ pythonTest/2.py
@@ -86,13 +86,143 @@ print(quadratic(0, 6, 6))
 
 
 # 计算次方,默认次幂为2
-def power(x, n=2):
+def power(b, n=2):
     s = 1
-    while n > 0 :
+    while n > 0:
         n = n-1
-        s = s*x
+        s = s*b
     return s
 
 
 print(power(3))
 
+
+def enroll(name, gender):
+    print('name:', name)
+    print('gender:', gender)
+
+
+print(enroll('wss', '3'))
+
+
+def enroll(name, gender, age=6, city='hangzhou'):
+    print('name: ', name)
+    print('gender: ', gender)
+    print('age: ', age)
+    print('city: ', city)
+
+
+print(enroll('wangsongsong', '6', '13', 'huainan'))
+
+
+def add_end(L=[]):
+    L.append('End')
+    return L
+
+
+print(add_end([1, 2, 3]))
+
+
+def add_end(L=None):
+    if L is None:
+        L = []
+    L.append('end')
+    return L
+
+
+print(add_end())
+
+
+# 计算a平方+b平方+...................
+def calc(numbers):
+    sum = 0
+    for n in numbers:
+        sum = sum + n *n
+    return sum
+
+
+# 调用上边的calc函数时, 需要预先组装一个list或tuple
+print(calc([1, 2, 3]))
+
+
+# 如果利用可变参数, 调用函数可以简化成calc(1, 2, 3)
+
+# 下面我们把函数的参数改为可变参数   加*后numbers内部接受到的是一个tuple,可以传入任意个参数,0个都可以
+def calc(*numbers):
+    sum = 0
+    for n in numbers:
+        sum = sum +n *n
+    return sum
+
+
+print(calc(1, 2, 3))
+
+
+# 如果已经有了一个list或者tuple, 要调用一个可变参数怎么办?
+# 可以如下  但是很麻烦
+nums = [1, 2, 3]
+print(calc(nums[0], nums[1], nums[2]))
+
+
+# 如上是可行的, 问题是太繁琐,所以Python允许你再list或tuple前面加一个*号, 把list或tuple的元素变成可变参数传入函数
+nums = [1, 2, 3]
+print(calc(*nums))
+# 如上 *nums表示把nums这个list的所有元素作为可变参数传入进入, 很常见很好用
+
+
+# 关键字参数
+# 可变参数允许你传入0个或任意个参数，这些可变参数在函数调用时自动组装为一个tuple。
+# 而关键字参数允许你传入0个或任意个含参数名的参数，这些关键字参数在函数内部自动组装为一个dict。
+def person(name, age, **kw):
+    print('name: ', name, 'age: ', age, 'other: ', kw)
+
+
+print(person('jack', 27, city='beijing', job='socker', ad='hang'))
+
+# 其上 kw可以不传入
+print(person('wss', 25))
+
+# kw也可以传入任意个关键字参数
+print(person('bob', 35, city='hangzhou', job='engineer'))
+# 利用**kw这个关键字参数可以扩展这个calc函数,多接收几个参数(可选)
+
+# 和可变参数类似, 也可以先事先组装一个dict, 然后, 把该dict转换为关键字参数传进去
+extra = {'city': 'guangzhou', 'job': 'whole'}
+print(person('nick', 48, city=extra['city'], job=extra['job']))
+# 上一句很麻烦, 可以这样写
+print(person('bitch', 23, **extra))
+
+# 总结:**extra的意思是把这个字典的所有key-value用关键字参数传入函数中**kw参数中
+# kw将获得一个字典, 注意这是获得一个复制而已
+
+
+# 命名关键字参数(因为上面的**kw不限制传入的参数,现在需要限制)
+# 如果要限制关键字参数的名字，就可以用命名关键字参数，例如，只接收city和job作为关键字参数。这种方式定义的函数如下：
+def person(name, age, *, city, job):
+    print(name, age, city, job)
+# 和关键字参数**kw不同，命名关键字参数需要一个特殊分隔符*，*后面的参数被视为命名关键字参数。
+
+
+print(person('jack', 27, city='beijing', job='socker'))
+
+
+# *args是可变参数, 可变参数后面的参数会成为关键字参数(关键字参数需要参数名, 如:city='hangzhou')
+def person(name, age, *args, city, job):
+    print(name, age, args, city, job)
+
+
+print(person('jack', 27, 1, 2, 3, city='beijing', job='socker'))
+# print(person('jack', 27, 1, 2, 4, 'beijing', 'socker'))
+
+
+# 参数组合
+# 在Python中定义函数，可以用必选参数、默认参数、可变参数、关键字参数和命名关键字参数，这5种参数都可以组合使用。
+# 但是请注意，参数定义的顺序必须是：必选参数、默认参数、可变参数、命名关键字参数和关键字参数。
+def f1(a, b, c=0, *args, **kw):
+    print('位置参数a =', a, '位置参数b =', b,
+          '默认参数c =', c,
+          '可变参数args=', args,
+          '关键字参数kw=', kw)
+
+
+print(f1(1, 2))
